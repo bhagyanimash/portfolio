fa413839e19e763f7421c72bda4c8e53da35269a
bhagyanimash <bhagyanimash2000@gmail.com>
2024-06-05 13:04:06 +0530

    build about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/kevin.jpg";
+import profilePic from "../assets/bnfull.png";
 
 function Hero() {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
-          <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin lg:mt-16 lg:text-8xl">
+          <div className="pl-16 flex flex-col items-center lg:items-start">
+            <h1 className="pb-16 text-6xl font-thin lg:mt-16 lg:text-7xl">
               Bhagya Nimash
             </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl tracking-tight text-transparent">
               Full Stack App Developer
             </span>
             <p className="my-2 max-w-xl py-6 font-ligh tracking-tighter">
@@ -21,7 +21,11 @@ function Hero() {
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img className="rounded-lg" src={profilePic} alt="Bhagya Nimash" />
+            <img
+              className="w-80 lg:w-96 rounded-2xl"
+              src={profilePic}
+              alt="Bhagya Nimash"
+            />
           </div>
         </div>
       </div>
