62689e967e6aa3247a248e0164eae61af903952d
bhagyanimash <bhagyanimash2000@gmail.com>
2024-06-05 20:39:05 +0530

    add motion to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/bnfull.png";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: { x: 0, opacity: 1, transition: { duration: 0.5, delay: delay } },
+});
 
 function Hero() {
   return (
@@ -8,21 +14,39 @@ function Hero() {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className=" flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin lg:mt-16 lg:text-7xl">
+            <motion.h1
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin lg:mt-16 lg:text-7xl"
+            >
               Hi, <br />
               I'm Bhagya Nimash
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-2xl tracking-tight text-transparent"
+            >
               Full Stack App Developer
-            </span>
-            <p className="my-2 max-w-xl py-6 font-ligh tracking-tighter">
+            </motion.span>
+            <motion.p
+              variants={container(1.5)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-ligh tracking-tighter"
+            >
               {HERO_CONTENT}
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
               className="w-80 lg:w-96 rounded-2xl"
               src={profilePic}
               alt="Bhagya Nimash"
