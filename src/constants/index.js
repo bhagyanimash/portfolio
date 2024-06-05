fc73d2c1a5e014bec5248b77032f62617091eef1
bhagyanimash <bhagyanimash2000@gmail.com>
2024-06-05 19:25:42 +0530

    add contact section

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "No 5, Kandy, Sri Lanka  ",
+  phoneNo: "+94 335 3306 ",
+  email: "nimashbhagya@gmail.com",
 };
